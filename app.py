82d9dd53d5e09fba5541c97a26242a166355bcbd
Mohamed Mamdouh <mohamedmamdouh1530@gmail.com>
2026-08-05 23:08:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def export_pdf(summary, history):
     pdf = UnicodePDF()
     pdf.add_page()
     
-    font_url = "https://github.com/google/fonts/raw/main/ofl/cairo/Cairo-Regular.ttf"
+    font_url = "https://github.com/google/fonts/raw/main/ofl/cairo/Cairo%5Bslnt%2Cwght%5D.ttf"
     response = requests.get(font_url)
     font_path = "Cairo-Regular.ttf"
     
